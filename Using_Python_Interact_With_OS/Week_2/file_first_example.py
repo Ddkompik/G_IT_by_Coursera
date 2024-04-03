bd2de4360e027dd1fe5e0a13b2eda5f38cdb3c7f
Dasha Komar Kamarova <59911690+Ddkompik@users.noreply.github.com>
2024-04-03 17:52:22 +0300

    Change The DOCSTRING -_-

--- Using_Python_Interact_With_OS/Week_2/file_first_example.py
+++ Using_Python_Interact_With_OS/Week_2/file_first_example.py
@@ -1,7 +1,7 @@
 from config import file_path
 
 def FileCheck(file_path):
-    '''Just using a method without Parameters To Check The File Exist Or Nope.
+    '''Just using a method To Check The File Exist Or Nope.
         If Yes , Than Just Read the Context In File
         Using DOCSTRING To Describe The Process Without Comments'''
     try:
@@ -9,11 +9,11 @@ def FileCheck(file_path):
             text.write("Hello")
             text.seek(0)
             text.read()
+
     except FileExistsError:
         with open(file_path, "r+", encoding="utf-8") as text:
             file = text.read()
             print(file)
-
 
 if __name__ == "__main__":
     print(FileCheck.__doc__)
