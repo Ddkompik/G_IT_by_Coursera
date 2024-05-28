00cc8f2c78f776d7b0843a9e2383bd66164bd311
Dasha Komar Kamarova <59911690+Ddkompik@users.noreply.github.com>
2024-05-28 16:21:01 +0300

    change the Flas for names

--- AutoPython/flusk_test/app.py
+++ AutoPython/flusk_test/app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 #content inside of the home dir
-@app.route("/")
+@app.route('/')
 
-def home():
-    return "Hello Flusk!"
+def show_user(username):
+    return f"Hello, {username}!"
+#its not a hack? 
+app.add_url_rule('/<username>', 'show_user', show_user) 
 
 if __name__ == "__main__":
     app.run(debug=True)
